ac290c58ec481798be86963c4f0dc239d94047d8
Nicolae Gherman <39433010+senpainikolay@users.noreply.github.com>
2022-03-14 15:36:47 +0200

    Add files via upload

---
+++ Lab3/src/token/token.go
@@ -0,0 +1,68 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENTIFIER"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN                  = "="
+	PLUS                    = "+"
+	MINUS                   = "-"
+	DIVISION          = "/"
+	MUTIPLICATION = "*"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA = ","
+
+	LPAREN = "LEFT_PAREN"
+	RPAREN = "RIGHT_PAREN"
+	LBRACE = "LEFT_BRACE"
+	RBRACE = "RIGHT_BRACE"
+
+	// Keywords
+	FUNCTION = "FUNCTION_DECLARION"
+	VAR      = "VAR_DECLARATION"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN" 
+	RETURN_BOOL = "RETURN_BOOL" 
+	RETURN_INT = "RETURN_INT"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"def":  FUNCTION,
+	"var":    VAR,
+	"True":   TRUE,
+	"False":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN, 
+       	"bool" : RETURN_BOOL, 
+	"int" : RETURN_INT, 
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
